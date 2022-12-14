da162da187664022a515f8559abed61da20e277d
koushikkrishna-github <koushikkrishna.boddu@gmail.com>
2022-12-14 22:41:43 +0530

    content @ homepage

--- php/contactForm.php
+++ php/contactForm.php
@@ -5,7 +5,7 @@ $subject = $_REQUEST["subject"];
 $msg   = $_REQUEST["msg"];
 $to    = "koushikkrishnakk007@gmail.com"; // ENTER YOUR EMAIL ADDRESS
 if (isset($email) && isset($name) && isset($msg)) {
-    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
+    $email_subject = "$name sent you a message via Masters Visa"; // ENTER YOUR EMAIL SUBJECT
 		$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
--- php/heroRegisterForm.php
+++ php/heroRegisterForm.php
@@ -5,7 +5,7 @@ $visa =  $_REQUEST["visa"];
 $country =  $_REQUEST["country"];
 $to    = "koushikkrishnakk007@gmail.com"; // ENTER YOUR EMAIL ADDRESS
 if (isset($email) && isset($email)) {
-    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
+    $email_subject = "$name sent you a message via Masters Visa"; // ENTER YOUR EMAIL SUBJECT
 		$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
